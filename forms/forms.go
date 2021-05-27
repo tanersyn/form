728d43d3865dbc1079add8f6cbc0528ecbdbdb4b
Taner Sayın <50462952+tanersyn@users.noreply.github.com>
2021-05-27 18:10:18 +0300

    Add files via upload

---
+++ forms/forms.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type ContactDetails struct {
+	Email   string
+	Subject string
+	Message string
+}
+
+func main() {
+	tmpl := template.Must(template.ParseFiles("forms.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			tmpl.Execute(w, nil)
+			return
+		}
+
+		details := ContactDetails{
+			Email:   r.FormValue("email"),
+			Subject: r.FormValue("subject"),
+			Message: r.FormValue("message"),
+		}
+
+		_ = details
+
+		tmpl.Execute(w, struct{ success bool }{true}) 
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
